0dbf397863302555823468c50fc05a79a6fefcce
estherpinos <estherpinos1@gmail.com>
2023-09-13 19:46:49 +0200

    Add[image, canvas, border,opacity]

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const slides= document.querySelector('.images');
+const groupImages= document.querySelector('.group-images');
 const btnTop = document.querySelector('.top');
 const btnDown = document.querySelector('.down');
 
@@ -11,5 +11,52 @@ const images=[
   'img/img/05.webp',
 ];
 
+groupImages.innerHTML = "";
+
+let conta = 0;
+for (let i = 0; i < images.length; i++) {
+  const image = images[i];
+  groupImages.innerHTML += `<img src="${image}" class="hiden">`;
+}
+
+let img = groupImages.getElementsByTagName("img");
+let canva = document.getElementsByClassName("black-canva");
+let bordo = document.getElementsByClassName("bordo");
+
+
+img[conta].classList.remove("hiden");
+canva[conta].classList.add("hiden");
+bordo[conta].style.border = "3px solid white";
+
+
+btnDown.addEventListener("click", function(){
+  img[conta].classList.add("hiden");
+  canva[conta].classList.remove("hiden");
+  bordo[conta].style.border = "";
+
+  conta++;
+  if(conta==5){
+    conta=0;
+  }
+  img[conta].classList.remove("hiden");
+  canva[conta].classList.add("hiden");
+  bordo[conta].style.border = "3px solid white";
+
+})
+
+btnTop.addEventListener("click", function(){
+  img[conta].classList.add("hiden");
+  canva[conta].classList.remove("hiden");
+  bordo[conta].style.border = "";
+  conta--;
+  if(conta==-1){
+    conta=4;
+  }
+  img[conta].classList.remove("hiden");
+  canva[conta].classList.add("hiden");
+  bordo[conta].style.border = "3px solid white";
+
+
+})
 
 
