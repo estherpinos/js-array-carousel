38b22b717e3de6f67ceb1a21a95f6fb8c1597398
estherpinos <estherpinos1@gmail.com>
2023-09-13 17:41:41 +0200

    Edit [structure]

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const slides= document.querySelector('.slides');
+const btnTop = document.querySelector('.top');
+const btnDown = document.querySelector('.down');
+
+const images=[
+  'img/img/01.webp',
+  'img/img/02.webp',
+  'img/img/03.webp',
+  'img/img/04.webp',
+  'img/img/05.webp',
+];
+
